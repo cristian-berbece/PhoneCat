0ecdc86614b26741f9d952d96e6483fbda7f78d3
Berbece Cristian <cristi.berbece@gmail.com>
2016-09-25 23:56:27 +0300

    Component first commit

--- app/app.js
+++ app/app.js
@@ -1,21 +1,20 @@
 var phonecatApp = angular.module('phonecatApp', []);
-
 //PhoneListController : 
-
 PhoneListController.$inject = ['$scope'];
 function PhoneListController($scope) {
 	$scope.message = "F**K YEAH";
 	$scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
+	    {
+	      name: 'Nexus S',
+	      snippet: 'Fast just got faster with Nexus S.'
+	    }, {
+	      name: 'Motorola XOOM™ with Wi-Fi',
+	      snippet: 'The Next, Next Generation tablet.'
+	    }, {
+	      name: 'MOTOROLA XOOM™',
+	      snippet: 'The Next, Next Generation tablet.'
+	    }
+  	];
 };
 phonecatApp.controller('PhoneListController',PhoneListController);
+

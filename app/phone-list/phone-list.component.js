a97a35aef4af7300310904d4bbf2c3cfdd86e8e5
Berbece Cristian <cristi.berbece@gmail.com>
2016-09-26 12:03:39 +0300

    added ordering to phone list

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -3,18 +3,24 @@ var module = angular.module('phoneList');
 
 
 var PhoneListController = function() {
-  this.phones = [
+	this.phones = [
         {
           name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 1,
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2,
+
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3,
         }
       ];
+    this.orderProp = 'age';
+
 };
 
 module.component('phoneList', {

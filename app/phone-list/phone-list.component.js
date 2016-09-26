449a5b332b2b5ad19fd0f9b9be3209659422a59b
Berbece Cristian <cristi.berbece@gmail.com>
2016-09-26 12:32:18 +0300

    nicely commented phone-list.component

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,7 +1,9 @@
-
 var module = angular.module('phoneList'); 
 
-
+/** PhoneListController 
+Controller used in the phone-list component
+Performs a nice GET request for the phones.json file
+*/
 var PhoneListController = function($http) {
 	var self = this;
 	
@@ -14,8 +16,18 @@ var PhoneListController = function($http) {
 	});
 };
 
+/**
+Dependency injection for PhoneListController
+$http: used to get phone list from the server 
+*/
 PhoneListController.$inject = ['$http'];
 
+
+/** phoneList Component
+To be deployed just by using <phone-list></phone-list>
+Displays a list of all phones in phones.json
+Performs filtering and ordering
+*/
 module.component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
     controller: PhoneListController,
